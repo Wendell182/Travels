98826a5e597a3352623a3ac3f4cfdf2989a5d38c
Wendell Claus Leite <wendellclaus@brq.com>
2022-08-02 23:47:17 -0300

    restart repo

---
+++ Travels/Model/TiposDeViagens.swift
@@ -0,0 +1,28 @@
+//
+//  TipoDeViagens.swift
+//  Travels
+//
+//  Created by user on 02/08/22.
+//
+
+import Foundation
+
+struct TiposDeViagens: Codable {
+    
+    var destaques: [Viagem]?
+    var ofertas: [Viagem]?
+    var internacionais: [Viagem]?
+    
+    static func jsonToData(_ json:[String: Any]) -> Data? {
+        return try? JSONSerialization.data(withJSONObject: json, options: [])
+    }
+
+    static func decodeJson(_ jsonData: Data) -> TiposDeViagens? {
+        do {
+            return try JSONDecoder().decode(TiposDeViagens.self, from: jsonData)
+        } catch {
+            print(error.localizedDescription)
+            return nil
+        }
+    }
+}
---
+++ Travels/ViewModel/ViagemViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  ViagemViewModel.swift
+//  Travels
+//
+//  Created by user on 02/08/22.
+//
+
+import Foundation
+
+enum ViagemViewModelType: String {
+    case destaques
+    case ofertas
+    case internacionais
+}
+
+protocol ViagemViewModel {
+    var tituloSessao: String { get }
+    var tipo: ViagemViewModelType { get }
+    var viagens: [Viagem] { get set }
+    var numeroDeLinhas: Int { get }
+}
