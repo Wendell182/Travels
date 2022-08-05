bdf4623a916649f75d11f5b8f0da04450a305af6
Wendell Claus Leite <wendellclaus@brq.com>
2022-08-05 09:31:32 -0300

    add DetailViewController

---
+++ Travels/View/DetalheViewController/DetalheViewController.swift
@@ -0,0 +1,15 @@
+//
+//  DetalheViewController.swift
+//  Travels
+//
+//  Created by user on 05/08/22.
+//
+
+import UIKit
+
+class DetalheViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
